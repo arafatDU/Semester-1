dabd8b2ebe063ff5835175d3718bcd1310e45aa5
ARAFAT <munshiarafathussain@gmail.com>
2023-11-18 19:44:35 +0600

    chapter 7 added

---
+++ CSE-101/chapter7/Example/7_5a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+double f_to_m(double f);
+
+int main() {
+	double feet;
+	
+	printf("Enter feet: ");
+	scanf("%lf", &feet);
+	printf("Meters: %f", f_to_m(feet));
+
+	return 0;
+}
+
+double f_to_m(double f) {
+	return f/3.28;
+}
